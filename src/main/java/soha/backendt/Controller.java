70f4fac759f7c0b9f372dcdd956d12279e4b1298
DESKTOP-FA3LODG\voslr <treesheep@naver.com>
2024-03-04 13:50:04 +0900

    FEAT : 로그 추가

--- src/main/java/soha/backendt/Controller.java
+++ src/main/java/soha/backendt/Controller.java
@@ -1,6 +1,7 @@
 package soha.backendt;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class Controller {
 
 /*    private final ProductRepository productRepository;
@@ -23,20 +25,24 @@ public class Controller {
 
     @GetMapping("/hello1")
     public ResponseEntity<String> hello1() {
+        log.info("GET : /api/hello1");
         return new ResponseEntity<>("Hello from hello1", HttpStatus.OK);
     }
 
     @GetMapping("/hello2")
     public ResponseEntity<String> hello2() {
+        log.info("GET : /api/hello2");
         return new ResponseEntity<>("Hello from hello2", HttpStatus.OK);
     }
 
     @GetMapping("/hello3")
     public ResponseEntity<String> hello3() {
+        log.info("GET : /api/hello3");
         return new ResponseEntity<>("Hello from hello3", HttpStatus.OK);
     }
     @GetMapping("/hello4")
     public ResponseEntity<String> hello4() {
+        log.info("GET : /api/hello4");
         return new ResponseEntity<>("Hello from hello4", HttpStatus.OK);
     }
 }
